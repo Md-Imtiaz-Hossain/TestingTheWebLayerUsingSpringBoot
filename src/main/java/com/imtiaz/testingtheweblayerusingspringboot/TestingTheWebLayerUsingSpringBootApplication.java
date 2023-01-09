d8d2cd9e246dba2320e943ed80d42108ceee1db2
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-01-09 07:09:13 +0600

    Initial commit. Added a controller.

---
+++ src/main/java/com/imtiaz/testingtheweblayerusingspringboot/TestingTheWebLayerUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.imtiaz.testingtheweblayerusingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTheWebLayerUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingTheWebLayerUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imtiaz/testingtheweblayerusingspringboot/TestingTheWebLayerUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imtiaz.testingtheweblayerusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingTheWebLayerUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
