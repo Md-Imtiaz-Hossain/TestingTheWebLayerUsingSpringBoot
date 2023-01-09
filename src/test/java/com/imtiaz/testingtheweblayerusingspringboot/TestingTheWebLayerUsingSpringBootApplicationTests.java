acc63bd64f20db33f344f9d1840a3b9d5b3fba4c
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-01-09 07:13:42 +0600

    WebApplication Test.

--- src/test/java/com/imtiaz/testingtheweblayerusingspringboot/TestingTheWebLayerUsingSpringBootApplicationTests.java
+++ src/test/java/com/imtiaz/testingtheweblayerusingspringboot/TestingTheWebLayerUsingSpringBootApplicationTests.java
@@ -1,13 +1,33 @@
 package com.imtiaz.testingtheweblayerusingspringboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestingTheWebLayerUsingSpringBootApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	//Test HTTP Request without starting server.
 	@Test
-	void contextLoads() {
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
 	}
 
 }
